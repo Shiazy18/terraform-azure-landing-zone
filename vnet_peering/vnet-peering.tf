2121d0dd639743a1b94e7ac165baf1a8efefcd66
Shivam <shivampant0998@gmail.com>
2025-08-11 12:45:31 +0530

    minor changes after folder move

--- vnet_peering/vnet-peering.tf
+++ vnet_peering/vnet-peering.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network_peering" "peering" {
     name = "peering-${var.source_virtual_network_name}-${var.destination_virtual_network_name}"
-    resource_group_name = azurerm_resource_group.network.name
+    resource_group_name = var.network_resource_group_name
     virtual_network_name = var.source_virtual_network_name
     remote_virtual_network_id = data.azurerm_virtual_network.destination.id
   
