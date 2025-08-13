7815471e2a468e6124a7ef5b8445e99e49346cc5
Shivam <shivampant0998@gmail.com>
2025-08-13 11:16:20 +0530

    added vars

--- nsg/variables.tf
+++ nsg/variables.tf
@@ -9,21 +9,12 @@ variable "location" {
   description = "Location where your modules will be created"
 }
 
-variable "route_table_name" {
+variable "network_security_group_name" {
     type = string
     description = "(Required) The name of the route table within which create the route. Changing this forces a new resource to be created."
 }
 
-variable "route_name" {
+variable "network_resource_group_name" {
   type = string
   description = "(Required) The name of the route. Changing this forces a new resource to be created."
-}
-variable "address_prefix" {
-  type = string
-  description = "(Required) The destination to which the route applies. Can be CIDR (such as 10.1.0.0/16) or Azure Service Tag (such as ApiManagement, AzureBackup or AzureMonitor) format."
-}
-
-variable "next_hop_type" {
-  type = string
-  description = "(Required) The type of Azure hop the packet should be sent to. Possible values are VirtualNetworkGateway, VnetLocal, Internet, VirtualAppliance and None."
 }
